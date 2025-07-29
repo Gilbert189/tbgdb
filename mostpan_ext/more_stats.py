d5cebdaca39acf8babc5ef3e12c849d5273808c7
Gilbert Dannelle Lo <gilbertdannellelo@gmail.com>
2025-07-29 23:29:45 +0700

    fix(server): make message_count() work
    - default_range["monthly"] uses 52 weeks (1 year)
    - set default cases for conditions
    - ranges throw an error when given bad values
    - use a more descriptive return value
    - SQL errors give 422 instead of 400

--- mostpan_ext/more_stats.py
+++ mostpan_ext/more_stats.py
@@ -14,6 +14,7 @@ from functools import partial
 logger = current_app.logger.getChild("more_stats")
 
 
+db = current_app.config.db
 api = g.blueprints.get("api", None)
 if api is not None:
     stats_api = Blueprint('stats', __name__)
@@ -36,7 +37,7 @@ if api is not None:
             "hourly": timedelta(days=1),
             "daily": timedelta(weeks=4),
             "weekly": timedelta(weeks=24),
-            "monthly": timedelta(weeks=50),
+            "monthly": timedelta(weeks=52),
         }
         try:
             if sample not in date_formats:
@@ -49,24 +50,32 @@ if api is not None:
                 f"user={int(uid)}"
                 for uid in args.getlist("user")
             )
+            if user_conditions == "":
+                user_conditions = "1"
             topic_conditions = " or ".join(
                 f"tid={int(tid)}"
                 for tid in args.getlist("topic")
             )
+            if topic_conditions == "":
+                topic_conditions = "1"
             board_conditions = " or ".join(
                 f"bid={int(bid)}"
                 for bid in args.getlist("board")
             )
-            start_range = args.get(
-                "start",
-                default=datetime.now() - default_range[sample],
-                type=datetime.fromisoformat,
-            )
-            end_range = args.get(
-                "end",
-                default=datetime.now(),
-                type=datetime.fromisoformat,
-            )
+            if board_conditions == "":
+                board_conditions = "1"
+            # args.get uses the default value on error (bad)
+            # so need to use a more convoluted method that does throw an error
+            start_range = args.get("start")
+            if start_range is None:
+                start_range = datetime.now() - default_range[sample]
+            else:
+                start_range = datetime.fromisoformat(start_range)
+            end_range = args.get("end")
+            if end_range is None:
+                end_range = datetime.now()
+            else:
+                end_range = datetime.fromisoformat(end_range)
             if end_range - start_range > range_limit[sample]:
                 raise ValueError("range exceeds limit")
             time_conditions = (
@@ -74,30 +83,40 @@ if api is not None:
                 f" and unixepoch(date) < {end_range.timestamp()}"
             )
 
-            cur = current_app.config.db.cursor()
+            cur = db.cursor()
             query = cur.execute(
                 f"""
-                select strftime(:datefmt, date) as time, count(*)
+                select strftime(:datefmt, date) as time, count(*) as count
                 from Messages
-                    join Boards using (tid)
-                where {user_conditions} or {topic_conditions}
-                    or {board_conditions} and {time_conditions}
+                    join Topics using (tid)
+                    join Boards using (bid)
+                where ({user_conditions}) and ({topic_conditions})
+                    and ({board_conditions}) and ({time_conditions})
                 group by time
                 """,
                 {"datefmt": date_formats[sample]}
             ).fetchall()
 
-            if query == []:
-                return query, 404
-            return query
-        except (ValueError, sqlite3.Error) as e:
+            return {
+                "conditions": {
+                    "user": user_conditions,
+                    "topic": topic_conditions,
+                    "board": board_conditions,
+                },
+                "start": start_range.isoformat(timespec="seconds"),
+                "end": end_range.isoformat(timespec="seconds"),
+                "counts": query,
+            }
+        except ValueError as e:
             return {type(e).__name__: str(e)}, 400
+        except sqlite3.Error as e:
+            return {type(e).__name__: str(e)}, 422
 
     current_app.config.other_api_examples.update({
         "message_counts_over_time":
         partial(url_for,
                 "api.stats.message_count",
-                sample="'hourly,daily,weekly,monthly'",
+                sample="hourly,daily,weekly,monthly",
                 user="...",
                 topic="...",
                 board="...",
