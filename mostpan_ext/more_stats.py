b3c6e064ead886d8e0b507dcee5e6276e645d891
Gilbert Dannelle Lo <gilbertdannellelo@gmail.com>
2025-08-01 13:04:43 +0700

    feat(more_utils)!: in message_count(), categories can be made seperate instead of summed together
    BREAKING CHANGE: output format is now different to compensate

--- mostpan_ext/more_stats.py
+++ mostpan_ext/more_stats.py
@@ -8,6 +8,7 @@ from flask import current_app, Blueprint, g, request, url_for
 from io import BytesIO
 from datetime import datetime, timedelta
 import sqlite3
+import re
 from functools import partial
 
 
@@ -38,6 +39,14 @@ if api is not None:
         "monthly": timedelta(weeks=52),
     }
 
+    def to_bool(x):  # noqa
+        try:
+            return bool(int(x))
+        except Exception:
+            return x in (
+                "true", "True", "TRUE", "T", "t", "yes", "y", "YES",
+            )
+
     @stats_api.route("/counts/<sample>")
     def message_count(sample):  # noqa
         try:
@@ -47,24 +56,35 @@ if api is not None:
                 )
 
             args = request.args
-            user_conditions = " or ".join(
+
+            # Assemble the user conditions
+            user_conditions = [
                 f"user={int(uid)}"
                 for uid in args.getlist("user")
-            )
-            if user_conditions == "":
-                user_conditions = "1"
-            topic_conditions = " or ".join(
+            ]
+            if user_conditions == []:
+                user_conditions = ["1"]
+            if args.get("combine_users", default=True, type=to_bool):
+                user_conditions = [" or ".join(user_conditions)]
+            # Assemble the topic conditions
+            topic_conditions = [
                 f"tid={int(tid)}"
                 for tid in args.getlist("topic")
-            )
-            if topic_conditions == "":
-                topic_conditions = "1"
-            board_conditions = " or ".join(
+            ]
+            if topic_conditions == []:
+                topic_conditions = ["1"]
+            if args.get("combine_topics", default=True, type=to_bool):
+                topic_conditions = [" or ".join(topic_conditions)]
+            # Assemble the board conditions
+            board_conditions = [
                 f"bid={int(bid)}"
                 for bid in args.getlist("board")
-            )
-            if board_conditions == "":
-                board_conditions = "1"
+            ]
+            if board_conditions == []:
+                board_conditions = ["1"]
+            if args.get("combine_boards", default=True, type=to_bool):
+                board_conditions = [" or ".join(board_conditions)]
+            # Assemble the time conditions
             # args.get uses the default value on error (bad)
             # so need to use a more convoluted method that does throw an error
             start_range = args.get("start")
@@ -84,27 +104,57 @@ if api is not None:
                 f" and unixepoch(date) < {end_range.timestamp()}"
             )
 
+            cumulative = args.get("cumulative", default=False, type=to_bool)
             count_criteria = (
                 "sum(count(*)) over (order by strftime(:datefmt, date))"
-                if args.get("cumulative", default=False, type=bool)
+                if cumulative
                 else "count(*)"
             )
 
+            result = {}
             cur = db.cursor()
-            query = cur.execute(
-                f"""
-                select
-                    strftime(:datefmt, date) as time,
-                    {count_criteria} as count
-                from Messages
-                    join Topics using (tid)
-                    join Boards using (bid)
-                where ({user_conditions}) and ({topic_conditions})
-                    and ({board_conditions}) and ({time_conditions})
-                group by time
-                """,
-                {"datefmt": DATE_FORMATS[sample]}
-            ).fetchall()
+            combinations = [
+                re.sub(
+                    r"1 and | and 1",
+                    "",
+                    f"{user} and {topic} and {board}"
+                )
+                for user in user_conditions
+                for topic in topic_conditions
+                for board in board_conditions
+            ]
+            for message_conditions in combinations:
+                query = cur.execute(
+                    f"""
+                    select
+                        strftime(:datefmt, date) as time,
+                        {count_criteria} as count
+                    from Messages
+                        join Topics using (tid)
+                        join Boards using (bid)
+                    where ({message_conditions}) and ({time_conditions})
+                    group by time
+                    """,
+                    {"datefmt": DATE_FORMATS[sample]}
+                ).fetchall()
+                for item in query:
+                    (result
+                     .setdefault(item["time"], {})
+                     .update({message_conditions: item["count"]})
+                     )
+            # Fill missing values with either:
+            # - zero if we're not taking a running sum, or
+            # - the last value if we're taking one
+            last_value = {}
+            for key_, point in sorted(result.items()):
+                last_value.update(point)
+                for message_conditions in combinations:
+                    point.setdefault(
+                        message_conditions,
+                        int(last_value[message_conditions])
+                        if cumulative
+                        else 0
+                    )
 
             return {
                 "conditions": {
@@ -114,7 +164,7 @@ if api is not None:
                 },
                 "start": start_range.isoformat(timespec="seconds"),
                 "end": end_range.isoformat(timespec="seconds"),
-                "counts": query,
+                "counts": result,
             }
         except ValueError as e:
             return {type(e).__name__: str(e)}, 400
